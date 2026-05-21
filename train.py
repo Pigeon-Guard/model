46b60a128cce1085c8f166d5456abe827c6d26c6
Jose Pablo Muñoz <josepablomunoz0@gmail.com>
2026-05-21 14:27:15 +0200

    Add metrics to callback

--- train.py
+++ train.py
@@ -3,30 +3,22 @@ from ultralytics.data.split import autosplit
 import wandb, argparse
 wandb.login()
 
-EXTRACTED_CONFIG: dict = {}
-
-def extract_config(trainer):
-    EXTRACTED_CONFIG['run_name'] = trainer.args.name
-    EXTRACTED_CONFIG['run_id'] = wandb.run.id
-
 def callback_fit_epoch_end(trainer):
     run = wandb.run
-    epoch = trainer.epoch
-    fitness_dict = {}
-    if trainer.fitness is not None:
-        fitness_dict = {"train/fitness": float(trainer.fitness)}
+    metrics_dict = {name: value for name, value in trainer.metrics.items()}
+    fitness_dict = {"metrics/fitness": trainer.fitness}
+    run.log({**metrics_dict, **fitness_dict})
 
-    run.log(
-        {**fitness_dict},
-        step=epoch,
-    )
-
-def callback_train_start(trainer):
+def callback_train_end(trainer):
     run = wandb.run
-    if run is None:
-        raise RuntimeError("W&B run is not initialized")
-
-    run.log({"ci_test/start_flag": 1})
+    artifact = wandb.Artifact(
+        name=f"model-{wandb.run.id}",
+        type="model",
+        description="Best checkpoint",
+        metadata={"best_fitness": trainer.best_fitness, **trainer.metrics},
+    )
+    artifact.add_file(str(trainer.best), name="best.pt")
+    run.log_artifact(artifact, aliases=["best"])
 
 def main(args):
     run = wandb.init(
@@ -42,10 +34,10 @@ def main(args):
 
     model = YOLO("yolov8n.pt")
 
-    model.add_callback("on_train_start", callback_train_start)
     model.add_callback("on_fit_epoch_end", callback_fit_epoch_end)
+    model.add_callback("on_train_end", callback_train_end)
 
-    model.train(data=args.data, epochs=args.epochs, imgsz=args.imgsz)
+    model.train(data=args.data, epochs=args.epochs, imgsz=args.imgsz, plots=False)
 
     metrics = model.val(data=args.data)
     latest_fitness = metrics.results_dict["fitness"]
@@ -53,12 +45,15 @@ def main(args):
     wandb_api = wandb.Api()
     new_best = False
 
+    import time
+    time.sleep(60)
+
     try:
         current_best_artifact = wandb_api.artifact(
             "wandb-registry-pigeon-guard/yolo-model:production"
         )
 
-        current_best_fitness = current_best_artifact.metadata.get("validation_fitness")
+        current_best_fitness = current_best_artifact.metadata.get("best_fitness")
         print(f"Current best fitness: {current_best_fitness}")
         if latest_fitness >= current_best_fitness:
             new_best = True
@@ -69,15 +64,15 @@ def main(args):
         new_best = True
 
     if new_best:
-        print("new best")
-        # artifact = wandb_api.artifact(f"pigeon-guard/ultralytics/run_{run.id}_model:best")
-        # artifact.metadata["validation_fitness"] = latest_fitness
-        # artifact.link("wandb-registry-pigeon-guard/yolo-model", aliases=["production"])
-        # artifact.save()
+        artifact = wandb_api.artifact(f"pigeon-guard/ultralytics/model-{run.id}:best")
+        artifact.metadata["best_fitness"] = latest_fitness
+        artifact.link("wandb-registry-pigeon-guard/yolo-model", aliases=["production"])
+        artifact.save()
+        print("Best model updated")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--data", default="dataset/data.yaml")
-    parser.add_argument("--epochs", type=int, default=2)
+    parser.add_argument("--epochs", type=int, default=1)
     parser.add_argument("--imgsz", type=int, default=224)
     main(parser.parse_args())
